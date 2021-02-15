e8ef994cdcbd12e2160d4fb03b444b052d721b43
kazuhiro.nishikawa <kazuhiro.nishikawa.96@gmail.com>
2021-02-16 07:30:15 +0900

    sorce-map

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './style.css';
 import './style.scss';
 
-console.log('I am log')
+alert('Hi, source-map!')
 
 ReactDOM.render(
   <div>Hello, React!!!!</div>,
